bb27f70dd25bea8e40aa83f3ca4d6c8bb9d4d847
wennshow <vladyr55555@gmail.com>
2024-10-15 17:04:37 +0300

    Creating Workout Reactive Form with Component

---
+++ fitnessWeb/src/app/components/workout/workout.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { UserService } from '../../service/user.service';
+
+@Component({
+  selector: 'app-workout',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './workout.component.html',
+  styleUrl: './workout.component.scss'
+})
+export class WorkoutComponent {
+
+  gridStyle = {
+    width: '100%',
+    textAlign: 'center'
+  };
+
+  workoutForm!: FormGroup;
+
+  listOfType: any[] = [
+    "Cardio",
+    "Strength",
+    "Flexibility",
+    "HIIT",
+    "Pilates",
+    "Dance",
+    "Swimming",
+    "Cycling",
+    "Running",
+    "Walking",
+    "Boxing",
+    "CrossFit",
+    "Rowing",
+    "Stretching",
+    "Martial Arts",
+    "Gymnastics",
+    "Climbing", 
+    "Plyometrics"
+  ];
+
+  constructor(private fb: FormBuilder,
+    private userService: UserService,
+    private message: NzMessageService,
+  ){
+
+  }
+
+  ngOnInit(){
+    this.workoutForm = this.fb.group({
+      type: [null, [Validators.required]],
+      duration: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      caloriesBurned: [null, [Validators.required]]
+    })
+  }
+
+}
