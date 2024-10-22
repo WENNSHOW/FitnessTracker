73e6d0dfd7c4d5cf923862512d946d1d893e6070
wennshow <vladyr55555@gmail.com>
2024-10-22 15:23:12 +0300

    Creating Workouts Chart on Angular Dashboard in Fitness Tracker app

--- fitnessWeb/src/app/components/dashboard/dashboard.component.ts
+++ fitnessWeb/src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { UserService } from '../../service/user.service';
 import { SharedModule } from '../../shared/shared.module';
+import Chart, { CategoryScale } from 'chart.js/auto';
+import { DatePipe } from '@angular/common';
+
+Chart.register(CategoryScale);
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  providers: [DatePipe]
 })
 export class DashboardComponent {
 
@@ -16,7 +21,12 @@ export class DashboardComponent {
   workouts:any;
   activities:any;
 
-  constructor(private userService: UserService){}
+  @ViewChild('workoutLineChart') private workoutLineChartRef: ElementRef;
+  @ViewChild('activityLineChart') private activityLineChartRef: ElementRef;
+
+  constructor(private userService: UserService,
+    private datePipe:DatePipe
+  ){}
 
   ngOnInit(){
     this.getStats();
@@ -28,7 +38,53 @@ export class DashboardComponent {
       this.workouts = res.workouts;
       this.activities = res.activities;
       console.log(this.workouts, this.activities);
+      if (this.workoutLineChartRef || this.workoutLineChartRef){
+        this.createLineChart();
+      }
     })
+  }
+
+  ngAfterViewInit(){
+    if(this.workouts && this.activities){
+      this.createLineChart();
+    }
+  }
+
+  createLineChart(){
+    const workoutCtx = this.workoutLineChartRef.nativeElement.getContext('2d');
+    const activityCtx = this.activityLineChartRef.nativeElement.getContext('2d');
+
+    new Chart(workoutCtx, {
+      type: 'line',
+      data: {
+        labels: this.workouts.map((data: {date:any;})=> this.datePipe.transform(data.date, 'MM/dd')),
+        datasets: [
+          {
+          label: 'Calories Burned',
+          data: this.workouts.map((data: {caloriesBurned:any;})=>data.caloriesBurned),
+          fill: false,
+            borderWidth: 2,
+            backgroundColor: 'rgba(80, 200, 120, 0.6)',
+            borderColor: 'rgba(0, 100, 0, 1)',
+        },
+        {
+          label: 'Duration',
+          data: this.workouts.map((data: {duration:any; })=> data.duration),
+          fill: false,
+            borderWidth: 2,
+            backgroundColor: 'rgba(120, 180, 200, 0.6)',
+            borderColor: 'rgba(0, 100, 150, 1)',
+        }
+      ]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
   }
 
   getStats(){
